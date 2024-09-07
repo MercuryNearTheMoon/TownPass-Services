db19cb9df610521d5dc3cc0e8233da0abacfaa04
JEFF Liao <jeffliao92316046@gmail.com>
2024-09-07 14:08:50 +0800

    feat :select chart

--- src/components/charts/LineChart.vue
+++ src/components/charts/LineChart.vue
@@ -1,40 +1,78 @@
 <script lang="ts" setup>
-    import ApexChart from 'vue3-apexcharts';
-    const chartOptions = {
-        chart: {
-            height: 350,
-            type: 'line',
-            zoom: {
-            enabled: false
-            }
-        },
-        dataLabels: {
-            enabled: false
-        },
-        stroke: {
-            curve: 'straight'
-        },
-        title: {
-            text: 'Product Trends by Month',
-            align: 'left'
-        },
-        grid: {
-            row: {
-            colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-            opacity: 0.5
-            },
-        },
-        xaxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
-        }
+import ApexChart from 'vue3-apexcharts';
+import { ref, defineProps, computed } from 'vue';
+
+// 定义 props
+const props = defineProps<{
+  chartSelect?: string;
+}>();
+
+// 定义数据集
+const data1 = [10, 41, 35, 51, 49, 62, 69, 91, 148];
+const data2 = [34, 10, 5, 51, 30, 72, 129, 51, 148];
+const data3 = [123, 120, 50, 24, 60, 10, 46, 170, 10];
+
+// 计算属性来动态获取数据
+const chartData = computed(() => {
+  switch (props.chartSelect) {
+    case 'chart1':
+      console.log(1);
+      return data1;
+    case 'chart2':
+      console.log(2);
+      return data2;
+    case 'chart3':
+      console.log(3);
+      return data3;
+    default:
+      return data1; // 默认情况
+  }
+});
+
+// 定义 x 轴分类
+const xaxis = ref(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']);
+
+// 计算属性来定义 series
+const series = computed(() => [
+  {
+    name: 'Desktops',
+    data: chartData.value // 使用计算属性的值
+  }
+]);
+
+// 定义图表选项
+const chartOptions = {
+  chart: {
+    height: 350,
+    type: 'line',
+    zoom: {
+      enabled: false
     }
-    const series = [{
-        name: "Desktops",
-        data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
-    }]
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    curve: 'straight'
+  },
+  title: {
+    text: 'Product Trends by Month',
+    align: 'left'
+  },
+  grid: {
+    row: {
+      colors: ['#f3f3f3', 'transparent'], // 每行的颜色
+      opacity: 0.5
+    }
+  },
+  xaxis: {
+    categories: xaxis.value // x 轴的分类
+  }
+};
 </script>
+
 <template>
-    <div id="chart">
-        <ApexChart type="line" height="350" :options="chartOptions" :series="series"></ApexChart>
-    </div>
+  <div id="chart">
+    <ApexChart type="line" height="350" :options="chartOptions" :series="series"></ApexChart>
+  </div>
 </template>
--- src/views/ChartTest.vue
+++ src/views/ChartTest.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
-    import LineChart from '@/components/charts/LineChart.vue';
-    import BaseButton from '@/components/atoms/BaseButton.vue';
+import LineChart from '@/components/charts/LineChart.vue';
+import BaseButton from '@/components/atoms/BaseButton.vue';
+import BaseSelect from '@/components/atoms/BaseSelect.vue';
+import type { Option } from '@/components/atoms/BaseSelect.vue';
+import { ref } from 'vue';
+const selectList: Option[] = [
+  { label: 'chart2', value: 'chart2' },
+  { label: 'chart3', value: 'chart3' }
+];
+const selectInput = ref('');
 </script>
 <template>
-    <LineChart></LineChart>
-    <div class="flex justify-center">
-      <BaseButton :outline="true" :link="true" routeInfo="/">
-        homebutton
-      </BaseButton>
-    </div>
+  <div>
+    <BaseSelect
+      v-model="selectInput"
+      selectId="chart_select"
+      :options="selectList"
+      defaultSelected="chart1"
+    >
+    </BaseSelect>
+  </div>
+  <LineChart :chartSelect="selectInput"></LineChart>
+  <div class="flex justify-center">
+    <BaseButton :outline="true" :link="true" routeInfo="/"> homebutton </BaseButton>
+  </div>
 </template>
